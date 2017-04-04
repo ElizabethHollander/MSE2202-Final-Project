bd506dcdce291d017cdc25b525b1912ee9c0b982
Desmond Davies <desi.davies@gmail.com>
2017-04-04 18:04:33 -0400

    Added servos library to files, worked on code to drop cube in chute

---
+++ Arduino1_Master_Arms/Release/Arduino1_Master_Arms.cpp.d
@@ -0,0 +1,22 @@
+C:\Users\Desi\AppData\Local\Temp\VMBuilds\Arduino1_Master_Arms\diecimila_atmega328\Release\Arduino1_Master_Arms.cpp.o: \
+ C:\Users\Desi\AppData\Local\Temp\VMBuilds\Arduino1_Master_Arms\diecimila_atmega328\Release\Arduino1_Master_Arms.cpp \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/arduino.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\variants\standard/pins_arduino.h \
+ C:\Users\Desi\Documents\Arduino\libraries\Servo\src/Servo.h \
+ C:\Users\Desi\Documents\Arduino\libraries\Servo\src/avr/ServoTimers.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\libraries\EEPROM\src/EEPROM.h \
+ C:\Users\Desi\Documents\Arduino\libraries\uSTimer2/uSTimer2.h \
+ C:\Users\Desi\Documents\Arduino\libraries\CharliePlexM/CharliePlexM.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\libraries\Wire\src/Wire.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ C:\Users\Desi\Documents\Arduino\libraries\I2CEncoder-master/I2CEncoder.h \
+ C:\Users\Desi\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\libraries\SoftwareSerial\src/SoftwareSerial.h
